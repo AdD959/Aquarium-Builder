6c69133563c0a9de9a5d44028592bf53b3ac207c
Adam Cummings <Adam_Cummings_@outlook.com>
2020-11-11 16:49:18 +0000

    my tank nav done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { SideBarService } from './shared/sidebar.service';
@@ -6,30 +6,8 @@ import { SideBarService } from './shared/sidebar.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
-  animations: [
-    trigger('inOutAnimation', [
-      transition(':enter', [
-        style({ width: 0 }),
-        animate('0.2s ease-out', style({ width: 350 })),
-      ]),
-      transition(':leave', [
-        style({ width: 350 }),
-        animate('0.2s ease-in', style({ width: 0 })),
-      ]),
-    ]),
-  ],
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  myTankExpanded = false;
 
-  constructor(private sideBarService: SideBarService) {
-    this.sideBarService.toggleSideBar.subscribe(() => {
-      this.myTankExpanded = true;
-    });
-  }
-
-  toggleMyTank() {
-    this.myTankExpanded = !this.myTankExpanded;
-  }
 }
--- src/app/my-tank-sidebar/my-tank-sidebar.component.ts
+++ src/app/my-tank-sidebar/my-tank-sidebar.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { SideBarService } from '../shared/sidebar.service';
 
 @Component({
   selector: 'app-my-tank-sidebar',
   templateUrl: './my-tank-sidebar.component.html',
-  styleUrls: ['./my-tank-sidebar.component.less']
+  styleUrls: ['./my-tank-sidebar.component.less'],
 })
 export class MyTankSidebarComponent implements OnInit {
+  myTankExpanded = false;
+  myTankLabelTriggered = false;
 
-  constructor() {
 
-   }
-
-  ngOnInit() {
+  constructor(private sideBarService: SideBarService) {
+    this.sideBarService.toggleSideBar.subscribe(() => {
+      this.myTankExpanded = true;
+    });
   }
 
+  ngOnInit() {}
+
+  openMyTank() {
+    this.myTankExpanded = true;
+  }
+
+  closeMyTank() {
+    this.myTankExpanded = false;
+  }
 }
